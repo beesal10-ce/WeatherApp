cbdd6e89e57faec221d860fd6413494cca9ccb7f
beesal10-ce <65252604+beesal10-ce@users.noreply.github.com>
2022-02-12 16:17:37 -0600

    Assignment 3

--- app/src/main/java/firstapp/weatherapp/MainActivity.kt
+++ app/src/main/java/firstapp/weatherapp/MainActivity.kt
@@ -10,11 +10,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var button: Button
 
+
+
+
     private lateinit var conditionIcon :ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     conditionIcon =findViewById(R.id.condition_icon)
+
+        button  = findViewById(R.id.button)
 
         button.setOnClickListener{
             startActivity(Intent(this, ForecastActivity::class.java))
